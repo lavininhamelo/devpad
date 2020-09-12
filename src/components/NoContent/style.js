cfb27f5a4928c66a6b36c752137c2dbd9869ebe5
ianito <iaan.developer@gmail.com>
2020-09-11 22:57:46 -0300

    no content component created

---
+++ src/components/NoContent/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  user-select: none;
+  color: #ccc;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: calc(100% - 80px);
+  text-align: center;
+`;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,7 +7,7 @@ import TagFilter from '../../components/TagFilter';
 import CardList from '../../components/CardList';
 import Search from '../../components/Search/index';
 import { Creators as EditorCreators } from '../../store/ducks/editor';
-
+import NoContent from '../../components/NoContent';
 export const Home = () => {
   const dispatch = useDispatch();
 
@@ -27,7 +27,7 @@ export const Home = () => {
       <Container>
         <Search />
         <TagFilter />
-        {filtered[0] ? <CardList cards={filtered}></CardList> : 'Nada aqui'}
+        {filtered[0] ? <CardList cards={filtered}></CardList> : <NoContent />}
       </Container>
     </>
   );
